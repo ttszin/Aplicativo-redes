c0f14fbdec45ab8bfe272803798227a9d526880c
Matheus Teixeira de Souza <matheuskrskr@gmail.com>
2023-10-15 23:49:20 -0300

    Esquema do mouse funcionando

--- Game/game.py
+++ Game/game.py
@@ -1,137 +1,69 @@
-import pygame,sys
-from pygame.locals import *
+import pygame
 import math
 
-
-
+# Defina as cores fora da classe
+color_1 = (222, 11, 11)  # Vermelho
+color_n = (195, 7, 237)  # Roxo
 
 class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.ImagemPlayer1 = pygame.image.load('./characters/fagames.png')
+        #self.ImagemPlayer1 = pygame.transform.scale(self.ImagemPlayer1, (64, 64))
 
-  def __init__(self):
-    pygame.sprite.Sprite.__init__(self)
-    self.ImagemPlayer1 = pygame.image.load('./characters/fagames.png')
+        # Colisoes
+        self.rect = self.ImagemPlayer1.get_rect()
+        self.rect.centerx = width // 2
+        self.rect.centery = height //2
+        
+        
 
-    #colisoes
-    self.rect = self.ImagemPlayer1.get_rect()
-    self.rect.centerx = width/2
-    self.rect.centerx = height-200
-
-
-    self.listaDisparo = []
-    self.vida = True
-
-  def disparar(self):
-    pass
-
-  def colocar(self, superficie):
-
-    superficie.blit(self.ImagemPlayer1, self.rect)
-  
-
-
-
-# position = pygame.img.get_rect()    ----- Corrigir o erro de fazer uma box para o personagem e tratar como player, pois a imagem não é possível pegar a posição
+    def colocar(self, superficie):
+        image = pygame.transform.rotate(self.ImagemPlayer1, angle)  # Rotaciona o personagem
+        image = pygame.transform.rotate(image,-33)  #Rotaciona o personagem para cima
+        rect = image.get_rect(center=self.rect.center)
+        superficie.blit(image, rect)
 
 def CS2():
+    pygame.init()
+    global width, height, angle
+    width = 1280
+    height = 720
+    angle = 0
+    screen = pygame.display.set_mode((width, height))
+    running = True
 
-  # pygame setup
-  pygame.init()
-  global width
-  width = 1280
-  global height
-  height = 720
+    jogador1 = Player()
 
-  screen = pygame.display.set_mode((width, height))
-  #clock = pygame.time.Clock()
-  running = True
-  angle = 0
-  scale = 1
-
-  jogador1 = Player()
-  
-  color_1 = "#de0b0b"
-  color_n = "#c307ed"
-
-  w, h = 600, 440
-
-
-  """
-
-  #Pega a imagem
-  image = pygame.image.load('./characters/fagames.png')
-  image = pygame.transform.rotate(image,-33)  #Rotaciona o personagem para cima
-  image.convert()
-
-  #Desenha um retangulo ao redor
-  rect = image.get_rect(center=(640,360))  #Posiciona o player no mapa
-  border_rect = image.get_rect()
-  pygame.draw.rect(image, color_1, border_rect, 1)
-
-  #Definindo o centro e a posição de mouse e teclado
-  center = width//2, height//2  #Pegando as coordenadas do centro da tela
-  mouse = pygame.mouse.get_pos()
-  keys = pygame.key.get_pressed()
-
-
-  img = image  #Armazenando a imagem em uma nova variável
-  rect = img.get_rect()  #Desenhando um retangulo ao redor da imagem
-  rect.center = center"""
-
-
-
-  while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+    center = width // 2, height // 2
     
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        running = False
-    
-    jogador1.colocar(screen)  
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    pygame.display.update()
-    
-    """
-      if event.type == pygame.MOUSEMOTION:
-        mouse = event.pos
+        # Atualiza a posição do mouse e calcula o ângulo de rotação
+        mouse = pygame.mouse.get_pos()
         x = mouse[0] - center[0]
         y = mouse[1] - center[1]
-    
         d = math.sqrt(x**2 + y**2)
         angle = math.degrees(-math.atan2(y, x))
-        img = pygame.transform.rotozoom(image, angle, scale)
-        #Construir retangulo ao redor da nova imagem
-        rect = img.get_rect()
-        rect.center = center
-      if event.type == keys[pygame.K_w]:
-        position.move(up=True)
-    
-    
 
-    screen.fill(color_n)
-    screen.blit(img, rect)
-    """
-    """
-    pygame.draw.rect(screen, color_n, rect, 3)
-    pygame.draw.line(screen, color_1, center, mouse, 2)
-    pygame.draw.circle(screen, color_1, center, 6, 1)
-    pygame.draw.circle(screen, color_n, mouse, 6, 2)
-    
-    
-    pygame.display.update()
-    """
-    
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("white")
+        screen.fill(color_n)
+        jogador1.colocar(screen)
 
-    # RENDER YOUR GAME HERE
-    #group_player.draw(screen)
+        # Desenhe o retângulo da hitbox
+        pygame.draw.rect(screen, "black", jogador1.rect, 3)
 
-    # flip() the display to put your work on screen
-    #pygame.display.flip()
+        # Desenhe a linha que segue o ponteiro
+        pygame.draw.line(screen, color_1, center, mouse, 2)
 
-    #clock.tick(60)  # limits FPS to 60
+        # Desenhe os círculos de referência
+        pygame.draw.circle(screen, color_1, center, 6, 1)
+        pygame.draw.circle(screen, "white", mouse, 6, 2)
 
-  pygame.quit()
+        pygame.display.update()
+
+    pygame.quit()
 
 CS2()
